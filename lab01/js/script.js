6ee2e0fea4894f903cc4482e080fba9f3005fd9a
Maria Soloveva <32188528+MariaSoloveva@users.noreply.github.com>
2019-10-25 17:51:53 +0300

    Update script.js

--- lab01/js/script.js
+++ lab01/js/script.js
@@ -1,54 +1,34 @@
-var doc_before = document.getElementById("before");
-var doc_after = document.getElementById("after");
-
-var before_Ctx = doc_before.getContext("2d");
-var after_Ctx = doc_after.getContext("2d");
-
-var imageElement = new Image();
-imageElement.src = "source/image.png";
-imageElement.onload = filterImage;
-
-function filterImage() {
-  var width = imageElement.naturalWidth;
-  var height = imageElement.naturalHeight;
-
-  function count(x, y, c) {
-    return (y * width + x) * 4 + c;
-  }
-
-  doc_before.width = width;
-  doc_before.height = height;
-  doc_after.width = width;
-  doc_after.height = height;
-
-  before_Ctx.drawImage(imageElement, 0, 0);
-  var image_before = before_Ctx.getImageData(0, 0, width, height);
-  var image_after = after_Ctx.createImageData(width, height);
-
-  function filter(x, y, oldData, newData) {
-    if (x === 0 || x === width - 1 || y === 0 || y === height - 1) {
-      for (var inner = 0; inner < 4; ++inner) {
-        var c = count(x, y, inner);
-        newData[c] = oldData[c];
+var canvas = document.getElementById("lab02");
+var ctx = canvas.getContext("2d");
+var img = new Image;
+img.setAttribute( 'crossOrigin', 'Anonymous');
+img.onload = function() {
+  ctx.drawImage(img, 0, 0);
+  var idata = ctx.getImageData(0, 0, canvas.width, canvas.height);
+  console.log(idata);
+  var canvas1 = document.getElementById("lab02_1");
+  var ctx1 = canvas1.getContext("2d");
+  var h = canvas.height;
+  var w = canvas.width;
+  var idata1 = ctx1.createImageData(w, h);
+  for (var i=1; i<h-1; i++) {
+    for (var j=1; j<w-1; j++) {
+      if (idata.data[(i*w+j)*4+3] != 0) {
+        idata1.data[(i*w+j)*4] = (idata.data[((i-1)*w+(j-1))*4] + idata.data[((i-1)*w+j)*4] + idata.data[((i-1)*w+(j+1))*4] +
+          idata.data[(i*w+(j-1))*4] + idata.data[(i*w+j)*4] + idata.data[(i*w+(j+1))*4] +
+          idata.data[((i+1)*w+(j-1))*4] + idata.data[((i+1)*w+j)*4] + idata.data[((i+1)*w+(j+1))*4]) / 9;
+        idata1.data[(i*w+j)*4+1] = (idata.data[((i-1)*w+(j-1))*4+1] + idata.data[((i-1)*w+j)*4+1] + idata.data[((i-1)*w+(j+1))*4+1] +
+          idata.data[(i*w+(j-1))*4+1] + idata.data[(i*w+j)*4+1] + idata.data[(i*w+(j+1))*4+1] +
+          idata.data[((i+1)*w+(j-1))*4+1] + idata.data[((i+1)*w+j)*4+1] + idata.data[((i+1)*w+(j+1))*4+1]) / 9;
+        idata1.data[(i*w+j)*4+2] = (idata.data[((i-1)*w+(j-1))*4+2] + idata.data[((i-1)*w+j)*4+2] + idata.data[((i-1)*w+(j+1))*4+2] +
+          idata.data[(i*w+(j-1))*4+2] + idata.data[(i*w+j)*4+2] + idata.data[(i*w+(j+1))*4+2] +
+          idata.data[((i+1)*w+(j-1))*4+2] + idata.data[((i+1)*w+j)*4+2] + idata.data[((i+1)*w+(j+1))*4+2]) / 9;
+        idata1.data[(i*w+j)*4+3] = (idata.data[((i-1)*w+(j-1))*4+3] + idata.data[((i-1)*w+j)*4+3] + idata.data[((i-1)*w+(j+1))*4+3] +
+          idata.data[(i*w+(j-1))*4+3] + idata.data[(i*w+j)*4+3] + idata.data[(i*w+(j+1))*4+3] +
+          idata.data[((i+1)*w+(j-1))*4+3] + idata.data[((i+1)*w+j)*4+3] + idata.data[((i+1)*w+(j+1))*4+3]) / 9;
       }
-      return;
-    }
-    for (var i = 0; i < 4; ++i) {
-      var summ = 0;
-      for (var dy = -1; dy <= 1; ++dy) {
-        for (var dx = -1; dx <= 1; ++dx) {
-          summ += oldData[count(x + dx, y + dy, i)];
-        }
-      }
-      newData[count(x, y, inner)] = summ / 9;
     }
   }
-
-  for (var y = 0; y < height; ++y) {
-    for (var x = 0; x < width; ++x) {
-      filter(x, y, image_before.data, image_after.data);
-    }
-  }
-
-  after_Ctx.putImageData(image_after, 0, 0);
-}
+  ctx1.putImageData(idata1, 0, 0);
+};
+img.src = "https://www.tutorialspoint.com/images/QAicon.png";
